3cd31e2b6e3fd4e908c392b1a2ab692d06ca3c4b
tranphat230806 <phat.tran09192005@gmail.com>
2026-05-13 20:56:35 +0700

    demo shoe_store

---
+++ ShoeStoreManagement/src/main/java/com/phat/shoestore/ShoeStoreManagementApplication.java
@@ -0,0 +1,13 @@
+package com.phat.shoestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeStoreManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoeStoreManagementApplication.class, args);
+	}
+
+}
---
+++ ShoeStoreManagement/src/test/java/com/phat/shoestore/ShoeStoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phat.shoestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeStoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
